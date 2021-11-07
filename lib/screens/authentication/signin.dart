257e6a0a421c4932aa80537a6c269fed915e1aa6
Malou <elmelund@hotmail.dk>
2021-11-07 02:04:02 +0100

    new signup design

--- lib/screens/authentication/signin.dart
+++ lib/screens/authentication/signin.dart
@@ -7,6 +7,7 @@ import 'package:waultar/navigation/screen.dart';
 import 'package:fluttericon/iconic_icons.dart';
 import 'package:waultar/widgets/logo.dart';
 import 'package:waultar/widgets/signup_form.dart';
+import 'package:waultar/widgets/signup_widget.dart';
 
 class SignInView extends StatefulWidget {
   final AppState _appState;
@@ -51,103 +52,118 @@ class _SignInViewState extends State<SignInView> {
 
   @override
   Widget build(BuildContext context) {
-    String pawIcon = 'lib/assets/graphics/Paw.svg';
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 24, 35, 39),
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(80, 50, 80, 50),
-            child: Row(
-              children: [
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        children: const [
-                          Text("Welcome to Waultar",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold)),
-                          Text(
-                              "A new private and personal data repository for individuals. From you Waultar dashboard, you can get an overview of all the personal data you have provided to different services you use (Facebook, eBay, VISA, etc.). \n\nVia Waultar, you can invoke your GDPR rights and be in full control of your personal data - Waultar will handle the logic and communication with the third-party services, such that you can safely redeem your GDPR rights without having to undergo complicated processes.",
-                              style: TextStyle(
-                                  height: 2,
-                                  letterSpacing: 0.5,
-                                  color: Color.fromARGB(255, 198, 201, 202),
-                                  fontSize: 12)),
-                          Divider(
-                            thickness: 1,
-                            color: Color.fromARGB(255, 198, 201, 202),
-                          ),
-                          ListTile(
-                            title: Text('Zero Knowledgement Ecryption',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Color.fromARGB(255, 198, 201, 202),
-                                )),
-                            leading: Icon(
-                              Icons.security,
-                              color: Colors.orange,
-                            ),
-                          ),
-                          ListTile(
-                            title: Text('GDPR rights enforcer',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Color.fromARGB(255, 198, 201, 202),
-                                )),
-                            leading: Icon(
-                              Icons.security,
-                              color: Colors.orange,
-                            ),
-                          ),
-                          ListTile(
-                            title: Text('Data management',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Color.fromARGB(255, 198, 201, 202),
-                                )),
-                            leading: Icon(
-                              Icons.security,
-                              color: Colors.orange,
-                            ),
-                          ),
-                        ])),
-                Expanded(flex: 1, child: Container()),
-                Expanded(
-                    flex: 2,
-                    child: Container(
-                        decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Padding(
-                            padding: const EdgeInsets.all(35.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [SignUpForm()],
-                            ))))
-              ],
-            )));
+        body: Container(
+      color: Colors.blue,
+      child: Stack(
+        children: [
+          const Positioned(
+              top: 20,
+              left: 20,
+              child: Text("WAULTAR",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24))),
+          Center(
+            child: Stack(clipBehavior: Clip.none, children: [
+              Positioned(
+                top: 100,
+                left: 500,
+                child: Image.asset(
+                  'lib/assets/graphics/Paws_blue.png',
+                  scale: 1.5,
+                ),
+              ),
+              SignUpWidget(_appState, _updateAppState)
+            ]),
+          )
+        ],
+      ),
+    ));
   }
 }
 
-                        // Padding(
-                        //     padding: const EdgeInsets.only(bottom: 30.0),
-                        //     child: Row(children: [
-                        //       CustomPaint(
-                        //         size: Size(
-                        //             40, (40 * 0.9016393442622951).toDouble()),
-                        //         painter: PawPainter(Colors.blue),
-                        //       ),
-                        //       const SizedBox(width: 15),
-                        //       const Text("Waultar",
-                        //           style: TextStyle(
-                        //               fontSize: 18,
-                        //               fontWeight: FontWeight.bold))
-                        //     ])),
+
+
+
+
+// Padding(
+//             padding: EdgeInsets.fromLTRB(80, 50, 80, 50),
+//             child: Row(
+//               children: [
+//                 Expanded(
+//                     flex: 2,
+//                     child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         mainAxisSize: MainAxisSize.max,
+//                         children: const [
+//                           Text("Welcome to Waultar",
+//                               textAlign: TextAlign.left,
+//                               style: TextStyle(
+//                                   color: Colors.white,
+//                                   fontSize: 32,
+//                                   fontWeight: FontWeight.bold)),
+//                           Text(
+//                               "A new private and personal data repository for individuals. From you Waultar dashboard, you can get an overview of all the personal data you have provided to different services you use (Facebook, eBay, VISA, etc.). \n\nVia Waultar, you can invoke your GDPR rights and be in full control of your personal data - Waultar will handle the logic and communication with the third-party services, such that you can safely redeem your GDPR rights without having to undergo complicated processes.",
+//                               style: TextStyle(
+//                                   height: 2,
+//                                   letterSpacing: 0.5,
+//                                   color: Color.fromARGB(255, 198, 201, 202),
+//                                   fontSize: 12)),
+//                           Divider(
+//                             thickness: 1,
+//                             color: Color.fromARGB(255, 198, 201, 202),
+//                           ),
+//                           ListTile(
+//                             title: Text('Zero Knowledgement Ecryption',
+//                                 style: TextStyle(
+//                                   fontSize: 14,
+//                                   color: Color.fromARGB(255, 198, 201, 202),
+//                                 )),
+//                             leading: Icon(
+//                               Icons.security,
+//                               color: Colors.orange,
+//                             ),
+//                           ),
+//                           ListTile(
+//                             title: Text('GDPR rights enforcer',
+//                                 style: TextStyle(
+//                                   fontSize: 14,
+//                                   color: Color.fromARGB(255, 198, 201, 202),
+//                                 )),
+//                             leading: Icon(
+//                               Icons.security,
+//                               color: Colors.orange,
+//                             ),
+//                           ),
+//                           ListTile(
+//                             title: Text('Data management',
+//                                 style: TextStyle(
+//                                   fontSize: 14,
+//                                   color: Color.fromARGB(255, 198, 201, 202),
+//                                 )),
+//                             leading: Icon(
+//                               Icons.security,
+//                               color: Colors.orange,
+//                             ),
+//                           ),
+//                         ])),
+//                 Expanded(flex: 1, child: Container()),
+//                 Expanded(
+//                     flex: 2,
+//                     child: Container(
+//                         decoration: const BoxDecoration(
+//                             color: Colors.white,
+//                             borderRadius:
+//                                 BorderRadius.all(Radius.circular(10))),
+//                         child: Padding(
+//                             padding: const EdgeInsets.all(35.0),
+//                             child: Column(
+//                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                               crossAxisAlignment: CrossAxisAlignment.start,
+//                               children: [SignUpForm(_appState, _updateAppState)],
+//                             ))))
+//               ],
+//             ))
