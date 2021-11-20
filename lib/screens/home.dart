85e619fe50346c345c99abd6cdb5c0b7be041ab8
MalouLandsgaard <elmelund@hotmail.dk>
2021-11-20 19:43:25 +0100

    added dashboard view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,7 +3,7 @@ import 'package:waultar/globals/scaffold_main.dart';
 import 'package:waultar/navigation/app_state.dart';
 import 'package:waultar/navigation/screen.dart';
 import 'package:waultar/widgets/dashboard.dart';
-import 'package:waultar/widgets/menu_panel1.dart';
+
 import 'package:waultar/widgets/menu_panel2.dart';
 import 'package:waultar/widgets/toppanel.dart';
 
@@ -34,6 +34,17 @@ class _HomePageViewState extends State<HomePageView> {
         child: const Text('Sign out'));
   }
 
+  Widget title() {
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        "Dashboard",
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return getScaffoldMain(
@@ -45,8 +56,9 @@ class _HomePageViewState extends State<HomePageView> {
             Container(
               height: MediaQuery.of(context).size.height,
               child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.start,
-                  children: [TopPanel(), Dashboard()]),
+                  children: [TopPanel(), title(), Dashboard()]),
             )
           ],
         ));
--- lib/widgets/dashboard.dart
+++ lib/widgets/dashboard.dart
@@ -10,10 +10,44 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 5,
-      height: 5,
-      color: Colors.green,
+    return Expanded(
+      child: Padding(
+        padding:
+            const EdgeInsets.only(left: 20.0, bottom: 20, right: 20, top: 0),
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                color: Color(0xFF1A1D1F),
+                width: MediaQuery.of(context).size.width - 125,
+                height: 200),
+            SizedBox(height: 20),
+            Container(
+                color: Color(0xFF1A1D1F),
+                width: MediaQuery.of(context).size.width - 125,
+                height: 200),
+            SizedBox(height: 20),
+            Container(
+                color: Color(0xFF1A1D1F),
+                width: MediaQuery.of(context).size.width - 125,
+                height: 200),
+          ],
+        )),
+      ),
     );
   }
 }
+
+// SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               title(),
+//               Container(color: Color(0xFF111111), width: 1000, height: 200),
+//               Container(color: Color(0xFF1B1E1F), width: 1000, height: 200),
+//               Container(color: Color(0xFFFFFFFF), width: 1000, height: 200)
+//             ],
+//           ),
+//         ),
+//       ),
