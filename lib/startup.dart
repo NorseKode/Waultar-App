46959ffe0c25043e503d234856d4348af7f04c39
gujo <gujo@itu.dk>
2022-01-31 23:04:33 +0100

    no need for an sqlite service

--- lib/startup.dart
+++ lib/startup.dart
@@ -1,34 +1,47 @@
 import 'dart:io';
-
+import 'dart:ffi';
+import 'package:sqlite3/open.dart';
 import 'package:drift/native.dart';
 import 'package:get_it/get_it.dart';
 import 'package:waultar/data/configs/drift_config.dart';
 import 'package:waultar/data/repositories/image_dao.dart';
 import 'package:waultar/data/repositories/appsettings_dao.dart';
 import 'package:waultar/domain/services/settings_service.dart';
-import 'package:waultar/domain/services/sqlite_service.dart';
-
-// package:waultar/data/repositories/appsettings_dao.dart
 
 final locator = GetIt.instance;
 
 Future<void> setupServices({bool testing = false}) async {
 
   // configure on startup the correct sqlite binary to use
-  SqliteService().init();
+  configureSQLiteBinaries();
 
   // when correct sqlite binary have initialized, register the db
   testing
-      ? locator.registerSingleton<WaultarDb>(WaultarDb.testing(NativeDatabase.memory()))
+      ? locator.registerSingleton<WaultarDb>(
+          WaultarDb.testing(NativeDatabase.memory()))
       : locator.registerSingleton<WaultarDb>(WaultarDb());
 
   var db = locator<WaultarDb>();
 
-  // settingsBox will be registered before SettingsService, to make the injection possible
-  locator.registerSingleton<AppSettingsDao>(db.appSettingsDao, instanceName: 'appSettingsDao');
+  locator.registerSingleton<AppSettingsDao>(db.appSettingsDao,
+      instanceName: 'appSettingsDao');
   locator.registerSingleton<ImageDao>(db.imageDao, instanceName: 'imageDao');
 
-  // services used throughout the app
   locator.registerSingleton<IAppSettingsService>(AppSettingsService(),
       instanceName: 'appSettingsService');
 }
+
+void configureSQLiteBinaries() {
+  open.overrideFor(OperatingSystem.windows, _openOnWindows);
+}
+
+DynamicLibrary _openOnWindows() {
+  final scriptDir = File(Platform.script.toFilePath()).parent;
+  final libraryNextToScript =
+      File('${scriptDir.path}/lib/assets/sqlite/sqlite3.dll');
+  return DynamicLibrary.open(libraryNextToScript.path);
+}
+
+  // TODO : make override for linux as well
+  // should be the same as for windows but with a sqlite3.so binary instead
+  // just place the .so file in assets next to the .dll
