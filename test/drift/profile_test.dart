eb860681c4229e919fddf0b2d471a911ca63852f
gujo <gujo@itu.dk>
2022-02-03 12:28:50 +0100

    prettier tests

--- test/drift/profile_test.dart
+++ test/drift/profile_test.dart
@@ -7,10 +7,12 @@ import 'package:waultar/startup.dart';
 
 Future<void> main() async {
   late WaultarDb db;
+  late IProfileRepository _repo;
   await setupServices(testing: true);
 
   setUpAll(() {
     db = locator<WaultarDb>();
+    _repo = db.profileDao;
   });
 
   tearDownAll(() async {
@@ -19,23 +21,17 @@ Future<void> main() async {
 
   group('Test that profile DAO invoked correct', () {
     test('given empty db returns no profile', () async {
-      IProfileRepository _dao = db.profileDao;
-
-      final profiles = await _dao.getAllProfiles();
+      final profiles = await _repo.getAllProfiles();
       expect(profiles.isEmpty, true);
-
-      // final created = await dao.getTodoByIdAsFuture(1);
-      // expect(created.title, 'Setup drift');
     });
 
     test('given new profile returns id 1', () async {
-      IProfileRepository _dao = db.profileDao;
       var model = ProfileModel(null, 'test_username', 'test_user', 'user@mail.com', 'male', 'he/she', '12345678', DateTime.now(), 'link', 'json');
 
-      int id = await _dao.addProfile(model);
+      int id = await _repo.addProfile(model);
       expect(id, 1);
 
-      final createdProfile = await _dao.getProfileById(1);
+      final createdProfile = await _repo.getProfileById(1);
       expect(createdProfile.name, 'test_user');
     });
   });
