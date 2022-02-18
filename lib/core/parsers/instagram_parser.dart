a2f7001e322314afcaadaa348b2e315641591329
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2022-02-18 16:33:08 +0100

    Started implementing final profile parse

--- lib/core/parsers/instagram_parser.dart
+++ lib/core/parsers/instagram_parser.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
+import 'package:flutter/rendering.dart';
 import 'package:tuple/tuple.dart';
 import 'package:waultar/configs/exceptions/parse_exception.dart';
 import 'package:waultar/configs/globals/app_logger.dart';
 import 'package:waultar/configs/globals/media_extensions.dart';
 import 'package:waultar/core/abstracts/abstract_parsers/base_parser.dart';
+import 'package:waultar/core/abstracts/abstract_repositories/i_profile_repository.dart';
 import 'package:waultar/core/models/index.dart';
+import 'package:waultar/core/models/model_helper.dart';
 import 'package:waultar/core/parsers/parse_helper.dart';
 import 'package:waultar/presentation/widgets/upload/upload_util.dart';
 import 'package:waultar/startup.dart';
@@ -13,21 +16,72 @@ import 'package:waultar/startup.dart';
 class InstagramParser extends BaseParser {
   var appLogger = locator.get<AppLogger>(instanceName: 'logger');
   ProfileModel? _profile;
+  var _profileFiles = ["personal_information", "signup_information", "profile_photos"];
 
   setProfile(ProfileModel profile) {
     _profile = profile;
   }
+  
+  Stream<ProfileModel> parseProfile(List<String> paths) async* {
+    ProfileModel profile = ParseHelper.profile;
+    var profileMain = paths.firstWhere((element) => element.contains(_profileFiles[0]));
+    paths.remove(profileMain);
+
+    await for (final object in parseFile(File(profileMain))) {
+      if (object is Map<String, dynamic> && object.containsKey("profile_user")) {
+        profile = ProfileModel.fromInstragram(object["profile_user"]);
+      }
+    }
+
+    var profileCreationDate = paths.firstWhere((element) => element.contains(_profileFiles[1]));
+    paths.remove(profileCreationDate);
+
+    await for (final object in parseFile(File(profileMain))) {
+      if (object is Map<String, dynamic> && object.containsKey("Time")) {
+        var datetime = ModelHelper.getTimestamp((object["Time"])["timestamp"]);
+        profile.createdTimestamp = datetime ?? DateTime.fromMicrosecondsSinceEpoch(0);
+      }
+    }
+
+    yield profile;
+  }
 
   @override
   Stream parseListOfPaths(List<String> paths) async* {
-    for (var path in paths) {
-      if (Extensions.isJson(path)) {
-        var file = File(path);
-        await for (final result in parseFile(file)) {
-          yield result;
-        }
-      }
-    }
+    // var profile = null;
+    // var profileMain = paths.firstWhere((element) => element.contains(_profileFiles[0]));
+    // paths.remove(profileMain);
+
+    // await for (final object in parseFile(File(profileMain))) {
+    //   if (object is Map<String, dynamic> && object.containsKey("profile_user")) {
+    //     profile = ProfileModel.fromInstragram(object["profile_user"]);
+    //   }
+    // }
+
+    // var profileCreationDate = paths.firstWhere((element) => element.contains(_profileFiles[1]));
+    // paths.remove(profileCreationDate);
+
+    // await for (final object in parseFile(File(profileMain))) {
+    //   if (object is Map<String, dynamic> && object.containsKey("Time")) {
+    //     var datetime = ModelHelper.getTimestamp((object["Time"])["timestamp"]);
+    //     profile.createdTimestamp = datetime ?? DateTime.fromMicrosecondsSinceEpoch(0);
+    //   }
+    // }
+
+    // var id = _profileRepo.addProfile(profile);
+    // profile = _profileRepo.getProfileById(id);
+    // _profile = profile;
+
+    // // TODO: profile picture
+
+    // for (var path in paths) {
+    //   if (Extensions.isJson(path)) {
+    //     var file = File(path);
+    //     await for (final result in parseFile(file)) {
+    //       yield result;
+    //     }
+    //   }
+    // }
   }
 
   @override
--- test/parser/parse_profile_test.dart
+++ test/parser/parse_profile_test.dart
@@ -12,8 +12,14 @@ import '../test_helper.dart';
 main() {
   var facebookProfile =
       File(path_dart.join(TestHelper.pathToCurrentFile(), "data", "facebook_profile.json"));
-  var instagramProfile =
-      File(path_dart.join(TestHelper.pathToCurrentFile(), "data", "instagram_profile.json"));
+  var instagramProfileMain =
+      File(path_dart.join(TestHelper.pathToCurrentFile(), "data", "personal_information.json"));
+  var instagramCreationDate =
+      File(path_dart.join(TestHelper.pathToCurrentFile(), "data", "signup_information.json"));
+  var instagramProfileFiles = [
+    instagramProfileMain,
+    instagramCreationDate,
+  ];
 
   setUpAll(() {
     TestHelper.clearTestLogger();
@@ -44,8 +50,27 @@ main() {
       expect(profile.metadata!.length, 9);
     });
 
+    test('Instagram get profile', () async {
+      var profile = (await InstagramParser()
+              .parseProfile(instagramProfileFiles.map((e) => e.path).toList())
+              .toList())
+          .first;
+      
+      expect(profile.bio, "REDACTED BIO");
+      expect(profile.fullName, "REDACTED NAME");
+      expect(profile.dateOfBirth, DateTime.utc(2021, 2, 12));
+      expect(profile.gender, "male");
+      // expect(profile.profilePicture!.uri.path,"media/other/33479950_2070043303318549_5971154643487555584_n_17935673491101223.jpg");
+      expect(profile.phoneNumbers!.length, 1);
+      expect(profile.username, "REDACTED USERNAME");
+      expect(profile.currentCity, "REDACTED CITY");
+      expect(profile.websites!.length, 1);
+      expect(profile.metadata!.length, 1);
+    });
+
     test('Instagram', () async {
-      var result = await InstagramParser().parseFile(instagramProfile).toList();
+      // var result = (await InstagramParser().parseListOfPaths(instagramProfileFiles.map((e) => e.path).toList()).toList()).first;
+      var result = await InstagramParser().parseFile(instagramProfileMain).toList();
 
       expect(result.length, 1);
 
