e035b49f74c0302da37919222aedb08769980018
gujo <gujo@itu.dk>
2022-02-18 11:36:29 +0100

    implemented file repo

--- lib/core/abstracts/abstract_repositories/i_video_repository.dart
+++ lib/core/abstracts/abstract_repositories/i_video_repository.dart
@@ -1,7 +1,7 @@
 import 'package:waultar/core/models/index.dart';
 
 abstract class IVideoRepository {
-  int addVideo(VideoModel image);
+  int addVideo(VideoModel video);
   VideoModel? getVideoById(int id);
   List<VideoModel>? getAllVideos();
   List<VideoModel>? getAllVideosByService(ServiceModel service);
