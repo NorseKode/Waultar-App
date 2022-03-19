b75f94028e3c3a0be830f3c181119137ea7bf95f
gujo <gujo@itu.dk>
2022-03-19 13:10:30 +0100

    updated objectbox to latest version

--- lib/core/inodes/datapoint_repo.dart
+++ lib/core/inodes/datapoint_repo.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:waultar/core/inodes/tree_nodes.dart';
+import 'package:waultar/core/models/ui_model.dart';
 import 'package:waultar/data/configs/objectbox.dart';
 import 'package:waultar/data/configs/objectbox.g.dart';
 
@@ -14,4 +17,46 @@ class DataPointRepository {
   List<int> addMany(List<DataPoint> dataPoints) => _dataBox.putMany(dataPoints);
   List<DataPoint> readAll() => _dataBox.getAll();
   int count() => _dataBox.count();
+  List<DataPoint> search(String searchString, int offset, int limit) {
+    var builder = _dataBox
+        .query(DataPoint_.searchString
+            .contains(searchString, caseSensitive: false))
+        .build();
+    builder
+      ..offset = offset
+      ..limit = limit;
+    return builder.find();
+  }
+}
+
+class UIDTO implements UIModel {
+
+  final String stringName;
+  final DataCategory category;
+  final Map<String, dynamic> dataMap;
+  
+  @override
+  Color getAssociatedColor() {
+    // TODO: implement getAssociatedColor
+    throw UnimplementedError();
+  }
+
+  @override
+  String getMostInformativeField() {
+    // TODO: implement getMostInformativeField
+    throw UnimplementedError();
+  }
+
+  @override
+  DateTime getTimestamp() {
+    // TODO: implement getTimestamp
+    throw UnimplementedError();
+  }
+
+  @override
+  Map<String, String> toMap() {
+    // TODO: implement toMap
+    throw UnimplementedError();
+  }
+
 }
--- lib/domain/services/text_search_service.dart
+++ lib/domain/services/text_search_service.dart
@@ -3,6 +3,7 @@ import 'package:waultar/core/abstracts/abstract_repositories/i_comment_repositor
 import 'package:waultar/core/abstracts/abstract_repositories/i_image_repository.dart';
 import 'package:waultar/core/abstracts/abstract_repositories/i_post_repository.dart';
 import 'package:waultar/core/abstracts/abstract_services/i_text_search_service.dart';
+import 'package:waultar/core/inodes/datapoint_repo.dart';
 import 'package:waultar/core/models/ui_model.dart';
 import 'package:waultar/startup.dart';
 
@@ -10,6 +11,7 @@ class TextSearchService extends ITextSearchService {
   final _postRepo = locator.get<IPostRepository>(instanceName: 'postRepo');
   final _imageRepo = locator.get<IImageRepository>(instanceName: 'imageRepo');
   final _commentRepo = locator.get<ICommentRepository>(instanceName: 'commentRepo');
+  final _dataRepo = locator.get<DataPointRepository>(instanceName: 'dataRepo');
 
   @override
   List<UIModel> search(Map<SearchCategories, bool> inputCategories, String search, int limit, int offset) {
@@ -26,6 +28,13 @@ class TextSearchService extends ITextSearchService {
         returnList.addAll(_commentRepo.search(search, offset, limit - 10));
       }
     });
+
+    return returnList;
+  }
+
+  List<UIModel> searchAll(String search, int offset, int limit) {
+    var returnList = <UIModel>[];
+
 
     return returnList;
   }
