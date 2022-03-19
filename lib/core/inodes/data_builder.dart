2c42867a2bb47d94f42bbd108ab77bd001cb7ed2
gujo <gujo@itu.dk>
2022-03-19 18:07:14 +0100

    databuilder initial implementation with image relations implemented - will do more testing of it now

--- lib/core/inodes/data_builder.dart
+++ lib/core/inodes/data_builder.dart
@@ -4,10 +4,7 @@ import 'package:waultar/configs/globals/media_extensions.dart';
 import 'package:waultar/core/inodes/media_documents.dart';
 import 'package:waultar/core/inodes/service_document.dart';
 import 'package:waultar/core/inodes/tree_nodes.dart';
-import 'package:waultar/data/entities/media/file_objectbox.dart';
-import 'package:waultar/data/entities/media/image_objectbox.dart';
-import 'package:waultar/data/entities/media/video_objectbox.dart';
-import 'package:waultar/data/entities/misc/service_objectbox.dart';
+import 'package:path/path.dart' as dart_path;
 
 class DataBuilder {
   late DataPoint dataPoint;
@@ -34,7 +31,7 @@ class DataBuilder {
   }
 
   void setData(String json) {
-    dataPoint.values = jsonEncode(json);
+    dataPoint.values = json;
   }
 
   DataPoint build() {
@@ -64,19 +61,34 @@ class DataBuilder {
     var videosFound = <VideoDocument>[];
     var filesFound = <FileDocument>[];
     var linksFound = <LinkDocument>[];
+    var timeStampsFound = <DateTime>[];
     // let's just use a nested funtion to recursively find our targets in the jsonMap
     recurse(dynamic json) {
       if (json is Map<String, dynamic>) {
         for (var entry in json.entries) {
           var value = entry.value;
           if (value is String) {
-            if (Extensions.isFile(value)) {
-
+            if (Extensions.isImage(value)) {
+              var image = ImageDocument(
+                uri: dart_path.normalize('$basePathToMedia/$value'),
+                data: jsonEncode(json),
+              );
+              image.relatedDatapoint.target = dataPoint;
+              image.service.target = dataPoint.service.target;
+              imagesFound.add(image);
             }
+
+            if (Extensions.isFile(value)) {}
+          } else {
+            recurse(entry.value);
           }
         }
       }
-      if (json is List<dynamic>) {}
+      if (json is List<dynamic>) {
+        for (var element in json) {
+          recurse(element);
+        }
+      }
     }
 
     recurse(json);
@@ -85,6 +97,5 @@ class DataBuilder {
     dataPoint.videos.addAll(videosFound);
     dataPoint.files.addAll(filesFound);
     dataPoint.links.addAll(linksFound);
-    
   }
 }
--- test/data_builder_test.dart
+++ test/data_builder_test.dart
@@ -1,6 +1,102 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:waultar/core/inodes/data_builder.dart';
+import 'package:waultar/core/inodes/service_document.dart';
+import 'package:waultar/core/inodes/tree_nodes.dart';
 
 import 'objectbox/setup.dart';
 
 Future<void> main() async {
   late final ObjectBoxMock _context;
+  final DataCategory testCategory =
+      DataCategory(name: 'TEST', matchingFolders: []);
+  final scriptDir = File(Platform.script.toFilePath()).parent;
+  final String fbBasePath = '${scriptDir.path}/test/objectbox/data/Facebook/';
+  final ServiceDocument fbService = ServiceDocument(
+      serviceName: 'Facebook', companyName: 'Meta', image: 'some path');
+
+  String fbpostWithOneImage = '''
+    {
+      "timestamp": 1354805114,
+      "attachments": [
+        {
+          "data": [
+            {
+              "media": {
+                "uri": "posts/media/your_posts/Landscape-Color-Test.jpg",
+                "creation_timestamp": 1354805114,
+                "media_metadata": {
+                  "photo_metadata": {
+                    "exif_data": [
+                      {
+                        "upload_ip": "192.192.192.128"
+                      }
+                    ]
+                  }
+                },
+                "title": "Mobiloverf\u00c3\u00b8rsler",
+                "description": "Sne sne!"
+              }
+            }
+          ]
+        }
+      ],
+      "data": [
+        {
+          "post": "Sne sne!"
+        }
+      ],
+      "title": "Malou Elmelund Landsgaard har tilf\u00c3\u00b8jet et nyt billede."
+    }
+  ''';
+
+  const String fbpostWithTwoImages = '''
+    {
+      "timestamp": 1273171899,
+      "attachments": [
+        {
+          "data": [
+            {
+              "media": {
+                "uri": "posts/media/your_posts/Landscape-Color-Test.jpg",
+                "creation_timestamp": 1273171899,
+                "title": "Mobile Uploads",
+                "description": "Flot landskab"
+              }
+            },
+            {
+              "media": {
+                "uri": "posts/media/your_posts/ITU-Test.jpg",
+                "creation_timestamp": 1273171899,
+                "title": "Mobile Uploads",
+                "description": "Det her er ITU"
+              }
+            }
+          ]
+        }
+      ],
+      "data": [
+        {
+          "post": "Tja hvad skal man sige"
+        }
+      ],
+      "title": "Lukas Vinther Offenberg Larsen added a new photos."
+    },
+  ''';
+  group('Facebook databuilder', () {
+    test(' - datapoint with one image relation', () {
+      var dataName = DataPointName(name: 'Test name');
+      var builder = DataBuilder(fbBasePath);
+
+      builder
+        ..setName(dataName)
+        ..setCategory(testCategory)
+        ..setService(fbService)
+        ..setData(fbpostWithOneImage);
+
+      final dataPoint = builder.build();
+      expect(dataPoint.images.isNotEmpty, true);
+    });
+  });
 }
