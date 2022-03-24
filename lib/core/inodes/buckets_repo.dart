b4d5fdaa0b73afa0ac51ab8b29263eb3b2adc898
gujo <gujo@itu.dk>
2022-03-24 17:22:06 +0100

    implemented bucket repo

--- lib/core/inodes/buckets_repo.dart
+++ lib/core/inodes/buckets_repo.dart
@@ -1,9 +1,19 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
+import 'package:tuple/tuple.dart';
+import 'package:waultar/core/inodes/tree_nodes.dart';
 import 'package:waultar/data/configs/objectbox.dart';
 import 'package:waultar/data/configs/objectbox.g.dart';
 import 'package:waultar/data/entities/timebuckets/buckets.dart';
 
 abstract class IBucketsRepository {
-  void createBuckets(DateTime dataPointsCreatedAfter);
+  Future createBuckets(DateTime dataPointsCreatedAfter);
+  List<YearBucket> getAllYears();
+  YearBucket? getYear(int year);
+  List<MonthBucket> getMonthsFromYearValue(int year);
+  List<MonthBucket> getMonthsFromYearId(int yearId);
+  List<DayBucket> getDaysFromMonth(MonthBucket month);
+  List<DayBucket> getDaysFromMonthId(int monthId);
 }
 
 class IRepository {
@@ -13,7 +23,6 @@ class IRepository {
 }
 
 class BucketsRepository extends IBucketsRepository {
-
   final ObjectBox _context;
   late final Box<YearBucket> _yearBox;
   late final Box<MonthBucket> _monthBox;
@@ -26,8 +35,127 @@ class BucketsRepository extends IBucketsRepository {
   }
 
   @override
-  void createBuckets(DateTime dataPointsCreatedAfter) {
-    throw UnimplementedError();
+  Future createBuckets(DateTime dataPointsCreatedAfter) async {
+    var createdSince = dataPointsCreatedAfter.microsecondsSinceEpoch;
+
+    var years = _yearBox.getAll();
+
+    // streams all datapoints that have been created after createdSince
+    var toBeProcessed = _context.store
+        .box<DataPoint>()
+        .query(DataPoint_.dbCreatedAt.greaterThan(createdSince))
+        .build()
+        .stream();
+
+    // process each datapoint from the stream
+    await for (var dataPoint in toBeProcessed) {
+      var timestamps = _scrapeUniqueTimestamps(dataPoint);
+
+      if (timestamps.isNotEmpty) {
+        int categoryId = dataPoint.category.targetId;
+        int serviceId = dataPoint.service.targetId;
+
+        for (var timestamp in timestamps) {
+          var dissected = _dissectDateTime(timestamp);
+          int year = dissected.item1;
+          int month = dissected.item2;
+          int day = dissected.item3;
+          int hour = dissected.item4;
+
+          var yearBucket = years.singleWhere((element) => element.year == year,
+              orElse: () => YearBucket(
+                  year: year, categoryCountMap: '{}', serviceCountMap: '{}'));
+        }
+      }
+    }
   }
-  
+
+  List<DateTime> _scrapeUniqueTimestamps(DataPoint dataPoint) {
+    var timestampsSet = <DateTime>{};
+    aux(dynamic data) {
+      if (data is Map<String, dynamic>) {
+        for (var value in data.values) {
+          aux(value);
+        }
+      }
+      if (data is List<dynamic>) {
+        for (var item in data) {
+          aux(item);
+        }
+      }
+      if (data is int || data is String) {
+        var timestamp = tryParse(data);
+        if (timestamp != null) timestampsSet.add(timestamp);
+      }
+    }
+
+    aux(dataPoint.asMap);
+    return timestampsSet.toList();
+  }
+
+  DateTime? tryParse(dynamic value) {
+    if (value is int) {
+      if (value <= 0) return null;
+
+      var length = '$value'.length;
+      if (length >= 10 && length < 12) // <== seconds
+        return DateTime.fromMillisecondsSinceEpoch(value * 1000);
+
+      if (length >= 12 && length < 15) // <== milliseconds
+        return DateTime.fromMillisecondsSinceEpoch(value);
+
+      if (length >= 15 && length < 23) // <== microseconds and nano
+        return DateTime.fromMicrosecondsSinceEpoch(value);
+    }
+
+    if (value is String) {
+      if (value.isEmpty) return null;
+      return DateTime.tryParse(value);
+    }
+
+    return null;
+  }
+
+  Tuple4<int, int, int, int> _dissectDateTime(DateTime timestamp) {
+    int year = timestamp.year;
+    int month = timestamp.month;
+    int day = timestamp.day;
+    int hour = timestamp.hour;
+    return Tuple4(year, month, day, hour);
+  }
+
+  @override
+  List<YearBucket> getAllYears() {
+    return _yearBox.getAll();
+  }
+
+  @override
+  List<DayBucket> getDaysFromMonth(MonthBucket month) {
+    var bucket = _monthBox.get(month.id);
+    return bucket == null ? [] : bucket.days;
+  }
+
+  @override
+  List<DayBucket> getDaysFromMonthId(int monthId) {
+    var bucket = _monthBox.get(monthId);
+    return bucket == null ? [] : bucket.days;
+  }
+
+  @override
+  List<MonthBucket> getMonthsFromYearId(int yearId) {
+    var bucket = _yearBox.get(yearId);
+    return bucket == null ? [] : bucket.months;
+  }
+
+  @override
+  List<MonthBucket> getMonthsFromYearValue(int year) {
+    var bucket =
+        _yearBox.query(YearBucket_.year.equals(year)).build().findUnique();
+    return bucket == null ? [] : bucket.months;
+  }
+
+  @override
+  YearBucket? getYear(int year) {
+    return _yearBox.query(YearBucket_.year.equals(year)).build().findUnique();
+  }
 }
