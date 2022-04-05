0ee12c228a4180fd965a3f6efcdc303e9b65bbdd
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2022-04-05 09:55:49 +0200

    Added functionality to image tagging widget

--- lib/presentation/widgets/dashboard/dashboard.dart
+++ lib/presentation/widgets/dashboard/dashboard.dart
@@ -13,7 +13,6 @@ import 'package:waultar/presentation/widgets/general/default_widgets/default_wid
 import 'package:waultar/presentation/widgets/general/default_widgets/service_widget.dart';
 import 'package:waultar/presentation/widgets/general/util_widgets/default_button.dart';
 
-
 import 'package:waultar/presentation/widgets/machine_models/image_classify_widget.dart';
 
 import 'package:waultar/startup.dart';
@@ -32,111 +31,60 @@ class _DashboardState extends State<Dashboard> {
   final IServiceRepository _serviceRepo =
       locator.get<IServiceRepository>(instanceName: 'serviceRepo');
 
-  final _mlService = locator.get<IMLService>(instanceName: 'mlService');
-  final _mediaRepo = locator.get<MediaRepository>(instanceName: 'mediaRepo');
-  var _imagesToTagCount = 0;
-  var _isLoading = false;
-
-  Widget _imageTaggingWidget() {
-    return DefaultWidget(
-        constraints: const BoxConstraints(maxWidth: 300),
-        title: "Image Classification",
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          const Text(
-            "Analyse the content of your images.",
-          ),
-          const SizedBox(height: 10),
-          Text("Untagged Images Count: $_imagesToTagCount"),
-          Text(
-              "Estimated Time To Tag All: ${(_imagesToTagCount * 0.5) / 60} minuets"),
-          const SizedBox(height: 10),
-          Divider(
-              height: 2,
-              thickness: 2,
-              color: themeProvider.themeMode().tonedColor),
-          const SizedBox(height: 10),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                DefaultButton(
-                  text: "       Tag Images       ",
-                  onPressed: () {
-                    setState(() {
-                      _isLoading = true;
-                    });
-
-                    _mlService.classifyImagesFromDB();
-
-                    setState(() {
-                      _isLoading = false;
-                    });
-                  },
-                ),
-              ],
-            ),
-          )
-        ]));
-  }
-
-
   @override
   Widget build(BuildContext context) {
     localizer = AppLocalizations.of(context)!;
     themeProvider = Provider.of<ThemeProvider>(context);
     final services = _serviceRepo.getAll();
 
-    List<Widget> serviceWidgets = List.generate(
-        services.length, (e) => ServiceWidget(service: services[e]));
+    List<Widget> serviceWidgets =
+        List.generate(services.length, (e) => ServiceWidget(service: services[e]));
 
     return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                localizer.dashboard,
-                style: themeProvider.themeData().textTheme.headline3,
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          localizer.dashboard,
+          style: themeProvider.themeData().textTheme.headline3,
+        ),
+        const SizedBox(height: 20),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SingleChildScrollView(
+                  //service widgets
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SingleChildScrollView(
-                        //service widgets
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: List.generate(
-                            serviceWidgets.length,
-                            (index) => Padding(
-                              padding: const EdgeInsets.only(right: 20),
-                              child: serviceWidgets[index],
-                            ),
-                          ),
-                        ),
+                    children: List.generate(
+                      serviceWidgets.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: serviceWidgets[index],
                       ),
-                      const SizedBox(height: 20),
-                      const ImageClassifyWidget(),
-                      const SizedBox(height: 20),
-                      Text(
-                        localizer.yourSocialDataOverview,
-                        style: themeProvider.themeData().textTheme.headline4,
-                      ), //dashboard widgets
-
-                      const SizedBox(height: 20),
-
-                      Row(), //expands window
-                      Wrap(
-                          spacing: 20,
-                          runSpacing: 20,
-                          children: [SentimentWidget(), _imageTaggingWidget()])
-
-                    ],
+                    ),
                   ),
                 ),
-              )
-            ],
-          );
+                const SizedBox(height: 20),
+                Text(
+                  localizer.yourSocialDataOverview,
+                  style: themeProvider.themeData().textTheme.headline4,
+                ), //dashboard widgets
+
+                const SizedBox(height: 20),
+
+                Row(), //expands window
+                Wrap(spacing: 20, runSpacing: 20, children: const [
+                  SentimentWidget(),
+                  ImageClassifyWidget(),
+                ])
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/presentation/widgets/machine_models/image_classify_widget.dart
+++ lib/presentation/widgets/machine_models/image_classify_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:waultar/core/abstracts/abstract_services/i_ml_service.dart';
 import 'package:waultar/data/repositories/media_repo.dart';
+import 'package:waultar/presentation/providers/theme_provider.dart';
 import 'package:waultar/presentation/widgets/general/default_widgets/default_widget.dart';
 import 'package:waultar/presentation/widgets/general/util_widgets/default_button.dart';
 import 'package:waultar/startup.dart';
@@ -16,10 +18,12 @@ class _ImageClassifyWidgetState extends State<ImageClassifyWidget> {
   @override
   Widget build(BuildContext context) {
     _imagesToTagCount = _mediaRepo.getAmountOfUnTaggedImages();
+    themeProvider = Provider.of<ThemeProvider>(context);
 
     return _mainBody();
   }
 
+  late ThemeProvider themeProvider;
   final _amountToTagTextController = TextEditingController();
   final _mlService = locator.get<IMLService>(instanceName: 'mlService');
   final _mediaRepo = locator.get<MediaRepository>(instanceName: 'mediaRepo');
@@ -62,25 +66,49 @@ class _ImageClassifyWidgetState extends State<ImageClassifyWidget> {
 
   Widget _mainBody() {
     return DefaultWidget(
+      constraints: const BoxConstraints(maxWidth: 300),
       title: "Image Classification",
       child: _isLoading
           ? _loadingScreen()
           : _imagesToTagCount == 0
               ? const Text("No Images To Tag")
               : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Untagged Images Count: $_imagesToTagCount"),
-                    TextField(
-                      keyboardType: TextInputType.number,
+                    const Text(
+                      "Analyse the content of your images.",
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter a number",
+                        label: Text("How many images to tag?"),
+                      ),
                       controller: _amountToTagTextController,
+                      keyboardType: TextInputType.number,
                     ),
-                    Text("Estimated Time To Tag All: ${(_imagesToTagCount * 0.2) / 60} minuets"),
-                    DefaultButton(
-                      text: "Tag Images",
-                      onPressed: () {
-                        _tagImages();
-                      },
+                    Text("Untagged Images Count: $_imagesToTagCount"),
+                    Text("Estimated Time To Tag All: ${(_imagesToTagCount * 0.4) / 60} minuets"),
+                    const SizedBox(height: 10),
+                    Divider(
+                      height: 2,
+                      thickness: 2,
+                      color: themeProvider.themeMode().tonedColor,
                     ),
+                    const SizedBox(height: 10),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          DefaultButton(
+                            text: "       Tag Images       ",
+                            onPressed: () {
+                              _tagImages();
+                            },
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
     );
