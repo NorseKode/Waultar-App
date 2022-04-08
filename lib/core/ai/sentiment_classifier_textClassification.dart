78469bcfb883c517d505be43ca9342079e23e941
gujo <gujo@itu.dk>
2022-04-08 13:44:34 +0200

    fixed tflite error with hardcoded tflite lib folder - we can now set it custom via the Interpreter.fromFile constructor

--- lib/core/ai/sentiment_classifier_textClassification.dart
+++ lib/core/ai/sentiment_classifier_textClassification.dart
@@ -1,14 +1,15 @@
+// ignore: file_names
 import 'package:path/path.dart' as path_dart;
-import 'package:waultar/assets/assets_helper.dart';
 import 'package:waultar/core/ai/sentiment_classifier.dart';
+import 'package:waultar/startup.dart';
 
 class SentimentClassifierTextClassifierTFLite extends SentimentClassifier {
-  SentimentClassifierTextClassifierTFLite({String? aiFolderPath})
+  SentimentClassifierTextClassifierTFLite()
       : super(
             modelPath: path_dart.join(
-                aiFolderPath ?? AssetsHelper.getPathToAILib(),
+                locator.get<String>(instanceName: 'ai_folder'),
                 "text_classification.tflite"), //text_classifitcation.tflite
             vocabPath: path_dart.join(
-                aiFolderPath ?? AssetsHelper.getPathToAILib(),
+                locator.get<String>(instanceName: 'ai_folder'),
                 "text_classification_vocab.txt"));
 }
