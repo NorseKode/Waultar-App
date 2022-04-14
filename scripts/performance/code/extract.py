c3560f0fcbf0195b858a81a1b90d7c2705535b47
LVOL98 <lukas98@live.dk>
2022-04-14 08:01:01 +0200

    Extract done

--- scripts/performance/code/extract.py
+++ scripts/performance/code/extract.py
@@ -1,5 +1,6 @@
 import json
 import os
+from .diagram_creator import *
 
 def extractAndParseReadingToConsole(path):
     extractData = json.load(open(path))
@@ -26,16 +27,24 @@ def extractAndParseReadingToConsole(path):
     
     extractionPercentage = extractTotalTime / totalTime * 100
     parsingPercentage = parseTime / totalTime * 100
-    totalPercentage = [extractionPercentage, parsingPercentage]
-    # totalLabels
+    other = 100 - extractionPercentage - parsingPercentage
+    totalPercentage = [extractionPercentage, parsingPercentage, other]
+    totalLabels = ["Extraction", "Parsing", "Other"]
+    # createPieChart(totalPercentage, totalLabels, "Extract and parsing time in percentage", "img/extract_and_parsev0.1.png")
 
-    # decodePercentage = 
-    # extractPercentage =
-    # extractSeries =
+    decodePercentage = decodeZipTime / extractTotalTime * 100
+    extractPercentage = extractedFileTime / extractTotalTime * 100
+    other = 100 - decodePercentage - extractPercentage
+    extractPercentage = [decodePercentage, extractPercentage, other]
+    extractLabel = ["Decode", "Extract", "Other"]
+    createPieChart(extractPercentage, extractLabel, "Percentage time in extraction", "img/extract_percentagev0.1.png")
+
+    parseFileTime = parseOfFileTme / parseTime * 100
+    extractSeries = [parseFileTime]
 
     print(f"Extraction and parsing took {totalTime / 100000} seconds")
     print(f"\tWith extraction taking {extractionPercentage}% of the time, with")
-    print(f"\t\tDecoding of zip taking {decodeZipTime / extractTotalTime * 100}% of the extraction time")
-    print(f"\t\tExtraction of file taking {extractedFileTime / extractTotalTime * 100}% of the extraction time")
+    print(f"\t\tDecoding of zip taking {decodePercentage}% of the extraction time")
+    print(f"\t\tExtraction of file taking {extractionPercentage}% of the extraction time")
     print(f"\tAnd parsing taking {parsingPercentage}% of the time, with")
-    print(f"\t\tParsing of file taking {parseOfFileTme / parseTime * 100}% of the parse time")
+    print(f"\t\tParsing of file taking {parseFileTime}% of the parse time")
